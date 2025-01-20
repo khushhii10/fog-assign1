5db8b31df4751f04e4a41fce420f3da8ea4ee5db
Khushi <khushi37yadav@gmail.com>
2025-01-21 01:21:06 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from "./components/Header";
+
+
+function Main() {
+    return (
+      <div>
+       <div><Header /></div>
+      </div>
+    );
+  }
+  export default Main;
